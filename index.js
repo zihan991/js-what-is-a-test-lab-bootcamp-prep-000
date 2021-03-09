2b73572d6a3928fd53016e581f24698734eb98e0
celestial-applet-1261 <zihan991@gmail.com>
2021-03-09 16:23:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // write your code below!
 var name = "Susan";
 var height = 38;
-var message = "gives the Susan and 38:";
+var message = `gives the ${name} and ${height}`;
 
 module.exports = {name, height, message};
